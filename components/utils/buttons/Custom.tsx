88a9177f70b09ae9f43c2d77714261b7818c786d
jis <spiroutecnikal@hotmail.com>
2024-08-13 22:06:26 +0200

    switch producer mode

---
+++ components/utils/buttons/Custom.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { GestureResponderEvent } from 'react-native'
+
+type CustomProps = {
+	label: string
+	extraClasses?: string
+	textClasses?: string
+	onPressFn: ((event: GestureResponderEvent) => void) | undefined
+}
+
+export default function Custom(props: CustomProps): JSX.Element {
+
+	return (
+		<TouchableOpacity 
+			className={
+				`
+					${props.extraClasses} 
+					 flex justify-center items-center w-min
+				`
+			} 
+			onPress={props.onPressFn}
+		>
+			<Text 
+				className={
+					`
+						${props.textClasses}
+						text-center
+					`}>
+						{props.label}
+					</Text>
+		</TouchableOpacity>
+	)
+}
--- screens/customer/Profil.tsx
+++ screens/customer/Profil.tsx
@@ -2,8 +2,10 @@ import { StyleSheet, View, SafeAreaView, Alert } from 'react-native';
 import React from "react";
 import { useColorScheme } from "nativewind";
 
+
 import Text from '../../components/utils/inputs/Text'
 import ButtonPrimaryEnd from '../../components/utils/buttons/PrimaryEnd'
+import Custom from '../../components/utils/buttons/Custom';
 import { useAuth } from '@clerk/clerk-expo'
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -18,7 +20,18 @@ import _Fontawesome from 'react-native-vector-icons/FontAwesome'
 const FontAwesome = _Fontawesome as React.ElementType
 
 
-export default function ProfilScreen() {
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../types/Navigation'
+type ProfileScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+};
+
+
+export default function ProfilScreen({ navigation }: Props) {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
   const dispatch = useDispatch()
@@ -33,6 +46,7 @@ export default function ProfilScreen() {
   const [firstname, setFirstname ] = useState('')
   const [lastname, setLastname ] = useState('')
 
+  const [graphicMode, setGraphicMode] = useState('Light')
  
 
   useEffect(() =>{
@@ -62,6 +76,22 @@ export default function ProfilScreen() {
       console.log(error)
     }
   }
+
+  const switchProducer = () => {
+    navigation.navigate('TabNavigatorProducer', {
+      screen: 'Accueil',
+    })
+  }
+
+  const toggleMode = () => {
+    if(graphicMode === 'Light') {
+      setGraphicMode('Dark')
+    } else {
+      setGraphicMode('Light')
+    }
+    toggleColorScheme()
+  }
+
   return (
 
     <SafeAreaView style={styles.container} className="bg-lightbg dark:bg-darkbg">
@@ -69,7 +99,8 @@ export default function ProfilScreen() {
       <TextHeading2 extraClasses="mb-5">Profil</TextHeading2>
       {
         isSignedIn && (
-          <View>
+          <View className="h-full relative flex items-center">
+            <View className="w-full">
             { user.clerkPasswordEnabled ? (
               <>
                 <Text 
@@ -96,7 +127,7 @@ export default function ProfilScreen() {
               </>
             ) : (
               <>
-                <View className="">
+                <View className="ml-2">
                   <TextBody2 extraClasses='font-bold text-secondary/60'>EMAIL</TextBody2>
                   <TextBody1 extraClasses="mb-5">{user.email}</TextBody1>
                 </View>
@@ -117,23 +148,30 @@ export default function ProfilScreen() {
               value={lastname}
               extraClasses='mb-2'
               ></Text>
-            <View className="rounded-full bg-darkbg flex justify-center items-center mb-5 w-[40px] h-[40px]">
-              <FontAwesome name="arrow-right" size={25} color="#FFFFFF"  className="absolute" />
+            <View className="rounded-full bg-warning flex justify-center items-center mb-5 w-[50px] h-[50px]">
+              <FontAwesome name="github-alt" size={35} color="#FFFFFF"  className="absolute" />
             </View>
             <ButtonPrimaryEnd 
               label="Enregistrer"
-              iconName="arrow-right"
+              iconName="save"
               disabled={false}
               onPressFn={() => handleRecord()}
               ></ButtonPrimaryEnd>
             <View className="mt-5">
               <ButtonPrimaryEnd
-                label="dark mode"
+                label={`version ${(graphicMode === 'Light') ? "Dark" : "Light"}`}
                 iconName="arrow-right"
                 disabled={false}
-                onPressFn={toggleColorScheme}
+                onPressFn={toggleMode}
               ></ButtonPrimaryEnd>
             </View>
+          </View>
+              <Custom
+                label="Basculer en mode Producteur"
+                extraClasses="bg-tertiary dark:bg-lightbg rounded-full px-5 absolute bottom-[250px] h-[60px]"
+                textClasses="text-lightbg dark:text-tertiary text-lg font-bold"
+                onPressFn={switchProducer}
+              ></Custom>
           </View>
           
         )
