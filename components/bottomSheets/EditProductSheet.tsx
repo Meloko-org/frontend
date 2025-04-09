2b99300f005f889b6eb954920bc73e14cebc8ae6
jis <jis@jisdev.com>
2025-04-09 11:10:45 +0200

    save before dev pull

---
+++ components/bottomSheets/EditProductSheet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function EditProductSheet() {
+  return (
+    <ActionSheet>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </ActionSheet>
+  );
+}
