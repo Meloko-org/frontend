0876c71ff8c174ba68f90432164b627ac8d40b82
jis <jis@jisdev.com>
2025-11-04 17:44:02 +0100

    review ProfileProducerScreen

--- screens/producer/Profile.tsx
+++ screens/producer/Profile.tsx
@@ -56,6 +56,7 @@ import Animated, {
 } from "react-native-reanimated";
 import TextHeading4 from "../../components/utils/texts/Heading4";
 import ColorSchemeButton from "../../components/utils/buttons/ColorScheme";
+import { useCollapsibleSection } from "../../hooks/useCollapsibleSection";
 
 type ProducerProfileNavProp = CompositeNavigationProp<
   BottomTabNavigationProp<ProducerTabParamList, "ProducerProfile">,
@@ -94,25 +95,18 @@ export default function ProducerProfileScreen({ navigation, route }: Props) {
   const { getToken, signOut } = useAuth();
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
-  // const [alertMessage, setAlertMessage] = useState<string | null>(null);
-  // const [alertType, setAlertType] = useState<"success" | "danger">("danger");
-
-  const [isOpenInfo, setOpenInfo] = useState(false);
-  const [isOpenAddress, setOpenAddress] = useState(false);
-
   const [isProducerSaveLoading, setProducerSaveLoading] = useState(false);
 
-  const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
-  // const bottomSheetRef = useRef<BottomSheet>(null);
+  const infoSection = useCollapsibleSection();
+  const addressSection = useCollapsibleSection();
 
-  // const openBottomSheet = () => {
-  //   bottomSheetRef.current?.expand();
-  // };
-
+  // infoSection
   const [socialReason, setSocialReason] = useState<string>("");
   const [siren, setSiren] = useState<string>("");
   const [iban, setIban] = useState<string>("");
   const [bic, setBic] = useState<string>("");
+
+  // addressSection
   const [address, setAddress] = useState({
     address1: "",
     address2: "",
@@ -120,31 +114,8 @@ export default function ProducerProfileScreen({ navigation, route }: Props) {
     city: "",
     country: "",
   });
+
   const [isPremium, setPremium] = useState<boolean>(true); // à modifier
-
-  const heightInfo = useSharedValue(0);
-  const heightAddress = useSharedValue(0);
-
-  const toggleOpenInfo = () => {
-    setOpenInfo((prev) => !prev);
-    heightInfo.value = isOpenInfo ? withTiming(0) : withTiming(330);
-    // heightInfo.value = isOpenInfo ? withTiming(infoMeasuredHeight) : withTiming(0);
-  };
-
-  const toggleOpenAddress = () => {
-    setOpenAddress((prev) => !prev);
-    heightAddress.value = isOpenAddress ? withTiming(0) : withTiming(380);
-  };
-
-  const animatedStyleInfo = useAnimatedStyle(() => ({
-    height: heightInfo.value,
-    opacity: heightInfo.value > 0 ? 1 : 0, // Facultatif : gérer l'opacité
-  }));
-
-  const animatedStyleAddress = useAnimatedStyle(() => ({
-    height: heightAddress.value,
-    opacity: heightAddress.value > 0 ? 1 : 0, // Facultatif : gérer l'opacité
-  }));
 
   useEffect(() => {
     console.log("Valeur actuelle de producerStore:", producerStore);
@@ -245,17 +216,21 @@ export default function ProducerProfileScreen({ navigation, route }: Props) {
   );
   console.log("USERSTORE -> ", userStore);
   console.log("PRODUCERSTORE -> ", producerStore);
-  console.log("SHOPSTORE -> ", shopStore);
+  console.log("SHOPSTORE -> ", shopStore?.isPremium);
   console.log("");
 
   return (
-    <SafeAreaView className="flex-1 bg-lightbg dark:bg-darkbg">
-      <View className="flex-1 items-center">
+    <SafeAreaView
+      className="flex-1 bg-lightbg dark:bg-darkbg"
+      edges={["right", "left", "top"]}
+    >
+      <View style={{ flex: 10 }}>
         <ScrollView
-          contentContainerStyle={{ flexGrow: 1 }}
+          contentContainerStyle={{ padding: 16, paddingBottom: 100 }}
           showsVerticalScrollIndicator={false}
-          className="flex h-full w-full p-3"
+          // className="flex h-full w-full p-3"
         >
+          {/* TopBar  */}
           <View className="flex flex-row items-center mb-5">
             <View className="">
               <ColorSchemeButton
@@ -282,129 +257,141 @@ export default function ProducerProfileScreen({ navigation, route }: Props) {
 
           <OpenMenuButton
             label="Informations"
-            onPressFn={toggleOpenInfo}
-            extraClasses="mb-3"
+            onPressFn={infoSection.toggle}
+            extraClasses="mb-2"
           />
 
           <Animated.View
-            style={[animatedStyleInfo]}
+            style={[infoSection.animatedStyle]}
             className="overflow-hidden"
           >
-            <InputText
-              label="Raison sociale"
-              placeholder="Saisissez votre raison sociale"
-              value={socialReason}
-              onChangeText={(value: string) => setSocialReason(value)}
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="SIREN"
-              placeholder="Saisissez votre Siren"
-              value={siren}
-              onChangeText={(value: string) => setSiren(value)}
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="IBAN"
-              placeholder="Saisissez votre Iban"
-              value={iban}
-              onChangeText={(value: string) => setIban(value)}
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="BIC"
-              placeholder="Saisissez votre Bic"
-              value={bic}
-              onChangeText={(value: string) => setBic(value)}
-              extraClasses="mb-2"
-            />
+            <View
+              onLayout={infoSection.onLayout}
+              style={infoSection.innerContainerStyle}
+              className="px-3"
+            >
+              <InputText
+                label="Raison sociale"
+                placeholder="Saisissez votre raison sociale"
+                value={socialReason}
+                onChangeText={(value: string) => setSocialReason(value)}
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="SIREN"
+                placeholder="Saisissez votre Siren"
+                value={siren}
+                onChangeText={(value: string) => setSiren(value)}
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="IBAN"
+                placeholder="Saisissez votre Iban"
+                value={iban}
+                onChangeText={(value: string) => setIban(value)}
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="BIC"
+                placeholder="Saisissez votre Bic"
+                value={bic}
+                onChangeText={(value: string) => setBic(value)}
+                extraClasses="mb-2"
+              />
+            </View>
           </Animated.View>
 
           <OpenMenuButton
             label="Adresse"
-            onPressFn={toggleOpenAddress}
-            extraClasses="mb-3"
+            onPressFn={addressSection.toggle}
+            extraClasses="mb-2"
           />
 
           <Animated.View
-            style={[animatedStyleAddress]}
+            style={[addressSection.animatedStyle]}
             className="overflow-hidden"
           >
-            <InputText
-              label="Adresse"
-              placeholder="Saisissez votre adresse"
-              value={address.address1}
-              onChangeText={(value: string) =>
-                setAddress({
-                  address1: value,
-                  address2: address.address2,
-                  postalCode: address.postalCode,
-                  city: address.city,
-                  country: address.country,
-                })
-              }
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="Adresse complément"
-              placeholder="Saisissez votre adresse"
-              value={address.address2}
-              onChangeText={(value: string) =>
-                setAddress({
-                  address1: address.address1,
-                  address2: value,
-                  postalCode: address.postalCode,
-                  city: address.city,
-                  country: address.country,
-                })
-              }
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="Code Postal"
-              placeholder="Saisissez le code postal"
-              value={address.postalCode}
-              onChangeText={(value: string) =>
-                setAddress({
-                  address1: address.address1,
-                  address2: address.address2,
-                  postalCode: value,
-                  city: address.city,
-                  country: address.country,
-                })
-              }
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="Ville"
-              placeholder="Saisissez la ville"
-              value={address.city}
-              onChangeText={(value: string) =>
-                setAddress({
-                  address1: address.address1,
-                  address2: address.address2,
-                  postalCode: address.postalCode,
-                  city: value,
-                  country: address.country,
-                })
-              }
-              extraClasses="mb-2"
-            />
-            <InputText
-              label="Pays"
-              placeholder="Saisissez le pays"
-              value={address.country}
-              onChangeText={(value: string) =>
-                setAddress({
-                  address1: address.address1,
-                  address2: address.address2,
-                  postalCode: address.postalCode,
-                  city: address.city,
-                  country: value,
-                })
-              }
-              extraClasses="mb-2"
-            />
+            <View
+              onLayout={addressSection.onLayout}
+              style={addressSection.innerContainerStyle}
+              className="px-3"
+            >
+              <InputText
+                label="Adresse"
+                placeholder="Saisissez votre adresse"
+                value={address.address1}
+                onChangeText={(value: string) =>
+                  setAddress({
+                    address1: value,
+                    address2: address.address2,
+                    postalCode: address.postalCode,
+                    city: address.city,
+                    country: address.country,
+                  })
+                }
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="Adresse complément"
+                placeholder="Saisissez votre adresse"
+                value={address.address2}
+                onChangeText={(value: string) =>
+                  setAddress({
+                    address1: address.address1,
+                    address2: value,
+                    postalCode: address.postalCode,
+                    city: address.city,
+                    country: address.country,
+                  })
+                }
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="Code Postal"
+                placeholder="Saisissez le code postal"
+                value={address.postalCode}
+                onChangeText={(value: string) =>
+                  setAddress({
+                    address1: address.address1,
+                    address2: address.address2,
+                    postalCode: value,
+                    city: address.city,
+                    country: address.country,
+                  })
+                }
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="Ville"
+                placeholder="Saisissez la ville"
+                value={address.city}
+                onChangeText={(value: string) =>
+                  setAddress({
+                    address1: address.address1,
+                    address2: address.address2,
+                    postalCode: address.postalCode,
+                    city: value,
+                    country: address.country,
+                  })
+                }
+                extraClasses="mb-2"
+              />
+              <InputText
+                label="Pays"
+                placeholder="Saisissez le pays"
+                value={address.country}
+                onChangeText={(value: string) =>
+                  setAddress({
+                    address1: address.address1,
+                    address2: address.address2,
+                    postalCode: address.postalCode,
+                    city: address.city,
+                    country: value,
+                  })
+                }
+                extraClasses="mb-2"
+              />
+            </View>
           </Animated.View>
 
           <View className="flex flex-row justify-center">
@@ -447,21 +434,16 @@ export default function ProducerProfileScreen({ navigation, route }: Props) {
         </ScrollView>
       </View>
 
-      <View className="absolute bottom-0 flex items-center w-full">
-        <CustomButton
-          label="Basculer en mode Utilisateur"
-          extraClasses="bg-tertiary dark:bg-lightbg rounded-full my-4 px-5 h-[60px]"
-          textClasses="text-lightbg dark:text-tertiary text-lg font-bold"
-          onPressFn={switchUser}
-        />
+      <View style={{ flex: 1.5 }}>
+        <View className="px-3">
+          <CustomButton
+            label="Basculer en mode Utilisateur"
+            extraClasses="bg-tertiary dark:bg-lightbg rounded-full my-4 px-5 h-[60px]"
+            textClasses="text-lightbg dark:text-tertiary text-lg font-bold"
+            onPressFn={switchUser}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-});
