5ca7512bf2c061b890d60420720a3a86780650ce
jis <jis@jisdev.com>
2025-12-05 12:08:42 +0100

    delete unused files

--- app.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  expo: require("./app.json").expo,
-};
--- patches/clerk-expo-disable-esm.js
+++
@@ -1,4 +0,0 @@
-// Empêche Clerk d'exécuter du code ESM pendant le prebuild Expo
-if (process.env.EXPO_PREBUILD) {
-  require = () => ({});
-}
